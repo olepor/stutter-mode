a773f4146cd32ff77d24121d673a46816de895fb
Ole P <olepor@matnat.uio.no>
2017-03-23 18:57:28 +0100

    Working with cons cell lists, not with normal lists

--- stutter-mode/stutter-tree.el
+++ stutter-mode/stutter-tree.el
@@ -34,35 +34,6 @@
    (t "message The car is neither cons nor integer - error!")))
 
 
-(defun minsert-str (element mlist)
-  (cond
-   ((consp (car mlist))
-    (progn
-      (message "The car of mlist is a cons cell")
-      ;; Check id's towards element id
-      (if (= (car element) (caar mlist))
-          (progn
-            (message "inserting with a matching id")
-            (minsert (cdr element) (car mlist)))
-        (if (cdr mlist)
-            ;; mlist has a cdr
-            (minsert element (cdr mlist))
-         (setcdr mlist (list element))))))
-   ((integerp (car mlist))
-    (progn
-      (message "The car of mlist is an integer")
-      ;; the element is either a function, or a cons-cell list
-      ;; the id's cdr is either a function or an mlist
-      (if (functionp element)
-          (setcdr mlist element)
-        (if (functionp (cdr mlist))
-            (setcdr mlist (list element))
-          ;; the element cdr is an mlist and so is the cdr of id
-          ;; Thus insert it into the mlist
-          (minsert element (cdr mlist))))))
-   (t "message The car is neither cons nor integer - error!")))
-
-
 mlist
 
 (cdadr mlist)
@@ -71,15 +42,8 @@ mlist
 
 (cdr mlist)
 
-(minsert (list 1 2 3 #'test-print) mlist)
 
-(minsert (cons 2 (cons 4 #'test-print)) mlist)
-
-(minsert (cons 2 #'test-print) mlist)
-
-(minsert (cons 3 #'test-print) mlist)
-
-(minsert (cons 4 #'test-print) mlist)
+(minsert (cons 1 (cons 2 #'test-print)) mlist)
 
 (setq mlist (list (cons 1  #'test-print)))
 
