92f8797897f257c776edff19a9386c7a03b7ba65
Ole P <olepor@matnat.uio.no>
2017-04-26 22:28:36 +0200

    Bulk upload.
    Given a shot at literate-programming in the style of big-D himself. I have
    therefore moved the code over to org-files, and provided plenty of commentary
    on the code, which frankly was needed. Have also provided a README file.

--- stutter-mode/latex-stutter-expansion-functions.el
+++ stutter-mode/latex-stutter-expansion-functions.el
@@ -1,3 +1,4 @@
+;; Requires YaSnippet
 
 ;; expands and sets point inside inline math-mode
 (defun latex-stutter-expand-inline-math-mode ()
@@ -6,3 +7,29 @@
   (backward-char 4)
   (delete-char -2)
   (forward-char 2))
+
+(defun latex-stutter-expand-math-mode ()
+  (interactive)
+  (delete-char -2)
+  (insert "\\\[")
+  (newline 2)
+  (insert "\\]")
+  (previous-line))
+
+(defun latex-stutter-expand-section-sub-section ()
+  (interactive)
+  (delete-char -2)
+  (if (not (char-equal (read-event "sub?") 106)) ;; 106 = ascii j
+      (progn
+        (insert "\\section*{}")
+        (backward-char))
+    (progn
+      (insert "\\subsection*{}")
+      (backward-char))))
+
+(defun latex-stutter-expand-begin-align ()
+  (interactive)
+  (delete-char -2)
+  (yas-expand-snippet (yas-lookup-snippet "begin-align" 'latex-mode)))
+
+
